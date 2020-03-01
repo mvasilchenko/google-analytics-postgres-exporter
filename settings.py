466cbb8a5bb3bd00f2d91a04a029de9ea67a5518
Mikhail Vasilchenko <m.vasilchenko@aitarget.com>
2020-03-01 17:57:45 +0300

    feat(settings): add settings file

---
+++ settings.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from typing import List
+
+import yaml
+
+
+@dataclass
+class GoogleAnalyticsCredentials:
+    client_id: str
+    private_key: str
+
+
+@dataclass
+class GoogleAnalyticsAccount:
+    account: str
+    webproperty: str
+    profile: str
+
+
+@dataclass
+class Metric:
+    metrics: List[str]
+    dimensions: List[str]
+    start_date: str
+    end_date: str
+
+
+class SettingsParser:
+    def __init__(self, yaml_file: str):
+        self.yaml_file = yaml.load(open(yaml_file))
+
+    def credentials_parser(self):
+        for record in self.yaml_file:
+            if record.get("id", False) == "credentials":
+                return GoogleAnalyticsCredentials(
+                    client_id=record["client_email"], private_key=record["private_key"]
+                )
+
+    def accounts_parser(self):
+        accounts = []
+        for record in self.yaml_file:
+            if record.get("id", False) == "ga settings":
+                accounts.append(
+                    GoogleAnalyticsAccount(
+                        account=record["account"],
+                        webproperty=record["webproperty"],
+                        profile=record["webproperty"],
+                    )
+                )
+        return accounts
+
+    def metrics_parser(self):
+        metrics = []
+        for record in self.yaml_file:
+            if record.get("metrics"):
+                metrics.append(Metric(**record["attributes"]))
+        return metrics
